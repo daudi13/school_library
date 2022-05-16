c1734c4f025a668c105d4bd85698cc4ed8c6a17a
daudi13 <davouma13@gmail.com>
2022-05-16 17:52:39 +0300

    Add class Teacher

--- teacher.rb
+++ teacher.rb
@@ -3,4 +3,10 @@ require './person.rb'
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    
+    @specialization = specialization
+  end
+  
+  def can_use_service? 
+    true
+  end
+end
