a8699e046ef6082028a309828021d676503a4609
daudi13 <davouma13@gmail.com>
2022-05-17 14:54:36 +0300

    rename file

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,5 +1,11 @@
 require './nameable'
 
 class Decorator < Nameable
-  
+  def initialize(nameable) 
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable
+  end
 end
---
+++ capitalize_decorator.rb
@@ -0,0 +1,3 @@
+require './base_decorator'
+
+class Capitalizd
