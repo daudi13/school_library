5ebf5c89d7126380a92f59707130c6e45b4636c3
daudi13 <davouma13@gmail.com>
2022-05-18 16:17:59 +0300

    refactor Rental constructor

--- rental.rb
+++ rental.rb
@@ -1,18 +1,12 @@
 class Rental
   attr_accessor :date 
-  attr_reader :book
+  attr_reader :book :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     super()
     @date = date
-  end
-
-  def book=(book)
     @book = book
     book.rental.push(self) unless book.rental.includes?(self)
-  end
-
-  def person=(person)
     @person = person
     person.rental.push(self) unless person.rental.includes?(self)
   end
