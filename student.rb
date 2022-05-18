a122e8730767816e29d8a56cdd3d24203c41b143
daudi13 <davouma13@gmail.com>
2022-05-18 14:49:43 +0300

    remove attr_accessor

--- student.rb
+++ student.rb
@@ -2,11 +2,10 @@ require './person'
 
 class Student < Person
   attr_reader :classroom
-  attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
+    @classroom = nil
   end
 
   def play_hooky
