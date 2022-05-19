dc3ade0738cfc59f5cf2bbf9e8b84cafbbf167a8
daudi13 <davouma13@gmail.com>
2022-05-19 15:47:02 +0300

    initialize  class App

--- app.rb
+++ app.rb
@@ -5,3 +5,20 @@ require './rental'
 require './teacher'
 require './student'
 
+class App
+  attr_accessor :client_input
+
+  def initialize
+    puts 'Welcome to School library App!'
+    ui_prompt
+    @rental = []
+    @people = []
+    @classroom_default = classroom.new('default-classroom')
+    @books = []
+    @client_input = gets.chomp
+  end
+
+  def ui_prompt 
+    puts 
+  end
+end
