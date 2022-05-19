e7c3ecc4e2d088dd5fde7a6cd66a1de5ab1cb044
daudi13 <davouma13@gmail.com>
2022-05-19 15:56:32 +0300

    Add ui_prompt method

--- app.rb
+++ app.rb
@@ -19,6 +19,17 @@ class App
   end
 
   def ui_prompt 
-    puts 
+    puts '
+    Please choose an option by entering a number:
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - list all rentals for a given person id
+    7 - Exit
+    '
+    puts ' '
+    puts 'Select an Option: '
   end
 end
